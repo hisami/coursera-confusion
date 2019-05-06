a785c329ce6db5741399ff3b60d49dd3e2b42d01
hisamitsuryouhei <koron.take@gmail.com>
2019-05-06 21:57:00 +0900

    Functional Component

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,25 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap'
 
 
-class DishDetail extends Component {
-    render() {
-        const dish = this.props.dish
-        if (dish) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        } else {
-            return (
-                <div></div>
-            );
-        }
+const DishDetail = (props) => {
+    const dish = props.dish
+    if (dish) {
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+    } else {
+        return (
+            <div></div>
+        );
     }
 }
 
